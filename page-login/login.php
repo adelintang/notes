56af73a7296dd3e1dc0a5254cb447a5e0b629855
adelintang <adelintang2001@gmail.com>
2022-09-08 16:44:40 +0700

    menambahkan tampilan login

---
+++ page-login/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="login-style.css?v=<?= time(); ?>">
+</head>
+
+<body>
+  <div class="container">
+    <div class="box">
+      <h1>Login</h1>
+      <div class="item">
+        <input type="text" placeholder="Masukkan username"><br>
+        <input type="password" placeholder="Masukkan password">
+      </div>
+      <button type="submit">Login</button>
+    </div>
+  </div>
+</body>
+
+</html>
